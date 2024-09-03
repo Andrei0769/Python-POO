0d2ee246f553a4aa7d7e4eb226c4742b60edce27
Andrei Luiz de jesus <159576658+Andrei0769@users.noreply.github.com>
2024-09-03 13:58:01 +0000

    update

--- python/6.  Encapsulamento.py
+++ python/6.  Encapsulamento.py
@@ -1,6 +1,12 @@
 import os 
 os.system("cls || clear ")
 
+# Criando sua própia exceção
+class SaldoInsuficienteError(Exception):
+    pass
+class ValorNegativoerror(Exception):
+    pass
+
 class Conta:
     def __init__(self, numero_conta: int, agencia: int) -> None:
         self.numero_conta = numero_conta
@@ -10,19 +16,36 @@ class Conta:
     @property 
     def saldo(self): # Para não precisar dos parantêses ()
         return self._saldo 
-    
-# função que faxz parte da classe se chama METODO
-    def sacar(self, valor):
-        if valor > self.saldo: # isto é uma validação
-            return f"Saldo Insuficiente"
+
+# FUNÇÃO DE SAQUE E # VERIFICAÇÃO DE TRY E EXCEPT
+    def sacar(self, valor) -> float:
+        try: 
+            self.__verificar_saque(valor)
+        except SaldoInsuficienteError as error:
+            return f"Error: {error}" 
+
         self._saldo -= valor
-        return self._saldo # retornando o valor após a validação do If 
+        return self._saldo 
     
+    def __verificar_saque(self,valor): #Metodo privado ////// # função para verificaar o saque 
+        if valor > self.saldo: # isto é uma validação
+            raise SaldoInsuficienteError(f"Saldo Insuficiente") # Capturando a excessão
+
+    # FUNÇÃO DE DEPOSITAR
     def depositar (self, valor):
-        if valor < 0:
-            return f"saldo invalido para depostitar"
+        # VERIFICAÇÃO DE TRY E EXCEPT
+        try:
+            self.__verificar_Deposito(valor)
+        except ValorNegativoerror as error:
+            return f"Error: {error}"
+        
         self._saldo = valor 
         return self._saldo
+    
+    # PUXANDO A VERIFICAÇÃO QUE FOI FEITA PELO TRY - EXCEPT
+    def __verificar_Deposito(self,valor):
+        if valor <0:
+            raise ValorNegativoerror("Não é possivel depositar valor negativo.")
 
 class ContaCorrente(Conta):
     pass
@@ -34,6 +57,7 @@ class ContaPoupanca(Conta):
 conta_corrente = ContaCorrente(222,333)
 conta_poupanca = ContaPoupanca(444,666)
 
+# Apresentado as variáveis do Codigo 
 print (conta_corrente.saldo)
-# print(conta_corrente.sacar(200))
-print(conta_corrente.depositar(-200))
+# print(conta_corrente.sacar(100))
+print(conta_corrente.depositar(-100))
